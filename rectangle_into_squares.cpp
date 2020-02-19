0e2c9dcf3b15cd7aaa0e2cd9ba52727feedda563
Abhinav Jhanwar <ajhanwar97@gmail.com>
2020-02-19 21:48:22 +0530

    rectangle into squares solution

---
+++ rectangle_into_squares.cpp
@@ -0,0 +1,24 @@
+// https://www.codewars.com/kata/55466989aeecab5aac00003e/cpp
+#include <vector>
+
+class SqInRect {
+public:
+  static std::vector<int> sqInRect(int lng, int wdth);
+};
+
+std::vector<int> SqInRect::sqInRect(int lng, int wdth) {
+  std::vector<int> solution;
+  int min;
+  if (lng == wdth) return solution;
+  while (lng != 0 && wdth != 0) {
+    if (lng < wdth) {
+      min = lng;
+      wdth -= min;
+    } else {
+      min = wdth;
+      lng -= min;
+    }
+    solution.emplace_back(min);
+  }
+  return solution;
+}
